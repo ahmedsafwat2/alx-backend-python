4a396f40d66fd8359b6e6ab2610f2cfcc0444413
Your Name <you@example.com>
2024-08-16 20:22:46 +0000

    add_mixed_list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@ Complex types - list of floats
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     return a sum of all nums inside a list
     """
